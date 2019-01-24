3b541fe893c2b3d55bd962ec88882b5fba16b803
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-24 14:50:54 +0000

    centred all text and forms

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,5 @@
 feature 'viewing hit points' do
-  scenario 'view player 2\'s hit points' do
+  scenario 'view player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content("Enemy: 100HP")
   end
